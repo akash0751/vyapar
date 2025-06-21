805c2e5f2ca9e56c78fc05443a811346edf47aab
akash0751 <akashsofficial05@gmail.com>
2025-06-21 17:21:39 +0530

    Deployed

--- Server/index.js
+++ Server/index.js
@@ -14,7 +14,7 @@ const app = express();
 
 // CORS Configuration
 app.use(cors({
-    origin: "http://localhost:5173",
+    origin: ["http://localhost:5173",'https://vyapar-plum.vercel.app'],
     methods: ["GET", "POST", "PUT", "DELETE"],
     credentials: true,
 }));
