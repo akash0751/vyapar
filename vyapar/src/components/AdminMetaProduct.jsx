c6dd89fe5605927b11cb608db1e6e80917330ad1
akash0751 <akashsofficial05@gmail.com>
2025-07-16 12:52:48 +0530

    added

--- vyapar/src/components/AdminMetaProduct.jsx
+++ vyapar/src/components/AdminMetaProduct.jsx
@@ -1,7 +1,6 @@
 import React, { useEffect, useState } from 'react';
 import { useNavigate } from 'react-router-dom';
 import { jwtDecode } from 'jwt-decode';
-import '../styles/AddProductMeta.css'; // Create this CSS for custom styling
 import adminAxiosInstance from '../utils/adminAxiosInstance';
 
 const AddProductMeta = () => {
