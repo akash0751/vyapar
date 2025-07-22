8ed37f5e994b8f4af41b5f9661a5a3240bc2481b
akash0751 <akashsofficial05@gmail.com>
2025-07-22 14:56:13 +0530

    added

--- Server/routes/productMeta.js
+++ Server/routes/productMeta.js
@@ -1,5 +1,6 @@
 const express = require('express')
 const router = express.Router()
+const ProductMeta = require('../Model/productMeta')
 const {addProductMeta, getProductMeta, updateProductMeta, deleteProductMeta} = require('../Controller/productMetaController')
 const {authenticateJWT} = require('../Middleware/Authorization')
 
@@ -9,10 +10,7 @@ router.put("/productMeta/:id", authenticateJWT, updateProductMeta);
 router.delete("productMeta/:id", authenticateJWT, deleteProductMeta);
 router.get('/productMeta/all', authenticateJWT, async (req, res) => {
   try {
-    const metas = await require('../Model/productMeta')
-      .find()
-      .populate('product', 'title')
-      .populate('addedBy', 'name');
+    const metas = await ProductMeta.find().populate('product', 'title').populate('addedBy', 'name');
     res.json({ success: true, meta: metas });
   } catch (err) {
     res.status(500).json({ success: false, error: err.message });
